8ffba4214508ce378491cc12e52f7a4982c3c70d
paranoidclark <clarkkemparan@gmail.com>
2024-02-29 21:18:43 +0800

    Added the Calculator UI

---
+++ script.js
@@ -0,0 +1,29 @@
+function addition(a, b) {
+    return a + b;
+}
+
+function subtraction(a, b) {
+    return a - b;
+}
+
+function multiplication(a, b) {
+    return a * b;
+}
+
+function division(a, b) {
+    return a / b;
+}
+
+function operate(firstNumber, operator, secondNumber) {
+
+}
+
+let display = document.querySelector("#display-txt")
+let seven = parseInt(document.querySelector("#seven").value)
+
+
+
+
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = "";
